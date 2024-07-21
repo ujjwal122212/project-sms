0f7e95755ba94cee67644cb2f84d91088101dc53
Ujjwal raj <ujjwalr658@gmail.com>
2024-07-21 11:10:32 +0530

    added

---
+++ src/app/student/fees/fee-page/fee-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fee-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './fee-page.component.html',
+  styleUrl: './fee-page.component.css'
+})
+export class FeePageComponent {
+
+}
---
+++ src/app/student/fees/fee-page2/fee-page2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fee-page2',
+  standalone: true,
+  imports: [],
+  templateUrl: './fee-page2.component.html',
+  styleUrl: './fee-page2.component.css'
+})
+export class FeePage2Component {
+
+}
