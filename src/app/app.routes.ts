e54214724f36bf685936a67d7a8f83b17daf794a
student-ranjankr <rkraj628715@gmail.com>
2024-07-22 12:56:51 +0530

    added quizes page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { ViewLeaveBookComponent } from './teacher/leave/view-leave-book/view-leave-book.component';
 
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PerformanceComponent } from './performance/performance.component';
+import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
-import { TimeTableComponent } from './time-table/time-table.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LeavePageComponent } from './teacher/leave/leave-page/leave-page.component';
-import { ApplyLeaveComponent } from './teacher/leave/apply-leave/apply-leave.component';
-import { ViewStatusComponent } from './teacher/leave/view-status/view-status.component';
-import { ViewBalanceComponent } from './teacher/leave/view-balance/view-balance.component';
-import { HolidayCalenderComponent } from './teacher/leave/holiday-calender/holiday-calender.component';
-import { CancelLeaveComponent } from './teacher/leave/cancel-leave/cancel-leave.component';
-import { UpcomingComponent } from './teacher/quiz/upcoming/upcoming.component';
 import { StudentHelpComponent } from './help/student-help.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PerformanceComponent } from './performance/performance.component';
+import { Quiz1Component } from './student/fees/quiz/quiz1/quiz1.component';
+import { ApplyLeaveComponent } from './teacher/leave/apply-leave/apply-leave.component';
+import { CancelLeaveComponent } from './teacher/leave/cancel-leave/cancel-leave.component';
+import { HolidayCalenderComponent } from './teacher/leave/holiday-calender/holiday-calender.component';
+import { LeavePageComponent } from './teacher/leave/leave-page/leave-page.component';
+import { ViewBalanceComponent } from './teacher/leave/view-balance/view-balance.component';
+import { ViewStatusComponent } from './teacher/leave/view-status/view-status.component';
+import { UpcomingComponent } from './teacher/quiz/upcoming/upcoming.component';
+import { TimeTableComponent } from './time-table/time-table.component';
 
 
 
@@ -35,6 +36,12 @@ export const routes: Routes = [
 
 { 'path': 'quizzes', 'title': 'quizzes', component:UpcomingComponent},
 
+//for tsudent page
+
+{ 'path': 'quiz1', 'title': 'quiz', component:Quiz1Component},
+
+
+
 { 'path': 'help', 'title': 'help', component:StudentHelpComponent},
   { 'path': '', 'title': 'Home', component:HomeComponent},
   { 'path': 'performance', 'title': 'Performance', component:PerformanceComponent },
@@ -46,8 +53,6 @@ export const routes: Routes = [
 
   { 'path': 'time', 'title': 'time-table', component:TimeTableComponent},
   { 'path': '**', component: NotFoundComponent },
-
-
 
 
 
---
+++ src/app/student/fees/quiz/quiz2/quiz2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz2',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz2.component.html',
+  styleUrl: './quiz2.component.css'
+})
+export class Quiz2Component {
+
+}
