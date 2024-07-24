e6d0e685ec6308f7df8f2bdaffd8269937ed7eae
Md Taufique <mdtaufique8084@gmail.com>
2024-07-24 12:52:20 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,23 @@
 import { ViewLeaveBookComponent } from './teacher/leave/view-leave-book/view-leave-book.component';
 
-import { Routes } from '@angular/router';
-import { CoursesComponent } from './courses/courses.component';
-import { StudentHelpComponent } from './help/student-help.component';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { PerformanceComponent } from './performance/performance.component';
-import { Quiz1Component } from './student/fees/quiz/quiz1/quiz1.component';
-import { ApplyLeaveComponent } from './teacher/leave/apply-leave/apply-leave.component';
-import { CancelLeaveComponent } from './teacher/leave/cancel-leave/cancel-leave.component';
-import { HolidayCalenderComponent } from './teacher/leave/holiday-calender/holiday-calender.component';
-import { LeavePageComponent } from './teacher/leave/leave-page/leave-page.component';
-import { ViewBalanceComponent } from './teacher/leave/view-balance/view-balance.component';
-import { ViewStatusComponent } from './teacher/leave/view-status/view-status.component';
-import { UpcomingComponent } from './teacher/quiz/upcoming/upcoming.component';
+import { CoursesComponent } from './courses/courses.component';
 import { TimeTableComponent } from './time-table/time-table.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LeavePageComponent } from './teacher/leave/leave-page/leave-page.component';
+import { ApplyLeaveComponent } from './teacher/leave/apply-leave/apply-leave.component';
+import { ViewStatusComponent } from './teacher/leave/view-status/view-status.component';
+import { ViewBalanceComponent } from './teacher/leave/view-balance/view-balance.component';
+import { HolidayCalenderComponent } from './teacher/leave/holiday-calender/holiday-calender.component';
+import { CancelLeaveComponent } from './teacher/leave/cancel-leave/cancel-leave.component';
+import { UpcomingComponent } from './teacher/quiz/upcoming/upcoming.component';
+import { StudentHelpComponent } from './help/student-help.component';
+import { TQuizComponent } from './teacher/quiz/t-quiz/t-quiz.component';
+import { OngoingComponent } from './teacher/quiz/ongoing/ongoing.component';
+import { SecheduleComponent } from './teacher/quiz/sechedule/sechedule.component';
+import { AssesmentComponent } from './teacher/quiz/assesment/assesment.component';
 
 
 
@@ -34,12 +37,15 @@ export const routes: Routes = [
 
 // for teacher quizess
 
-{ 'path': 'quizzes', 'title': 'quizzes', component:UpcomingComponent},
+{ 'path': 'quizzes', 'title': 'quizzes', component:TQuizComponent},
 
-//for tsudent page
-
-{ 'path': 'quiz1', 'title': 'quiz', component:Quiz1Component},
-
+{ 'path': 'quizzes', children :[
+  
+  { 'path': 'upcoming', 'title': 'upcoming-quizzes', component:UpcomingComponent},
+  { 'path': 'ongoing', 'title': 'ongoing-quizzes', component:OngoingComponent},
+  { 'path': 'schedule', 'title': 'schedule-quizzes', component:SecheduleComponent},
+  { 'path': 'Assesment', 'title': 'Assesment-quizzes', component:AssesmentComponent},
+]},
 
 
 { 'path': 'help', 'title': 'help', component:StudentHelpComponent},
@@ -53,6 +59,8 @@ export const routes: Routes = [
 
   { 'path': 'time', 'title': 'time-table', component:TimeTableComponent},
   { 'path': '**', component: NotFoundComponent },
+
+
 
 
 
