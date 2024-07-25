39b6964244b9af37a1b887ee7b919eb09d36aaea
student-ranjankr <rkraj628715@gmail.com>
2024-07-25 13:19:48 +0530

    added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,22 @@
 import { ViewLeaveBookComponent } from './teacher/leave/view-leave-book/view-leave-book.component';
 
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { PerformanceComponent } from './performance/performance.component';
+// import { PerformanceComponent } from './performance/performance.component';
 import { CoursesComponent } from './courses/courses.component';
-import { TimeTableComponent } from './time-table/time-table.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { PerformanceComponent } from './performance/performance.component';
-import { Quiz1Component } from './student/fees/quiz/quiz1/quiz1.component';
-import { ApplyLeaveComponent } from './teacher/leave/apply-leave/apply-leave.component';
-import { ViewStatusComponent } from './teacher/leave/view-status/view-status.component';
-import { ViewBalanceComponent } from './teacher/leave/view-balance/view-balance.component';
-import { HolidayCalenderComponent } from './teacher/leave/holiday-calender/holiday-calender.component';
-import { CancelLeaveComponent } from './teacher/leave/cancel-leave/cancel-leave.component';
-import { UpcomingComponent } from './teacher/quiz/upcoming/upcoming.component';
+import { TimeTableComponent } from './time-table/time-table.component';
+// import { Quiz1Component } from './student/fees/quiz/quiz1/quiz1.component';
 import { StudentHelpComponent } from './help/student-help.component';
-import { TQuizComponent } from './teacher/quiz/t-quiz/t-quiz.component';
-import { OngoingComponent } from './teacher/quiz/ongoing/ongoing.component';
-import { SecheduleComponent } from './teacher/quiz/sechedule/sechedule.component';
-import { AssesmentComponent } from './teacher/quiz/assesment/assesment.component';
+import { Quiz1Component } from './student/quiz/quiz1/quiz1.component';
+import { ApplyLeaveComponent } from './teacher/leave/apply-leave/apply-leave.component';
+import { CancelLeaveComponent } from './teacher/leave/cancel-leave/cancel-leave.component';
+import { HolidayCalenderComponent } from './teacher/leave/holiday-calender/holiday-calender.component';
+import { LeavePageComponent } from './teacher/leave/leave-page/leave-page.component';
+import { ViewBalanceComponent } from './teacher/leave/view-balance/view-balance.component';
+import { ViewStatusComponent } from './teacher/leave/view-status/view-status.component';
+import { UpcomingComponent } from './teacher/quiz/upcoming/upcoming.component';
 
 
 
