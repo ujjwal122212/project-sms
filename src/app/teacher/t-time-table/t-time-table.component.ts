7ffda9c472821c55a39a663cd155b540a0f30de1
Alok Kumar Singh <i.am.alok2513@gmail.com>
2024-07-26 10:31:43 +0530

    teacher time table component added

--- src/app/teacher/t-time-table/t-time-table.component.ts
+++ src/app/teacher/t-time-table/t-time-table.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-t-time-table',
   standalone: true,
-  imports: [],
+  imports: [RouterLink ,RouterLinkActive],
   templateUrl: './t-time-table.component.html',
   styleUrl: './t-time-table.component.css'
 })
