08eb754ceabf0bfd72e7ab679cf3a55ae85ea5b8
Md Taufique <mdtaufique8084@gmail.com>
2024-07-27 14:50:25 +0530

    add all

---
+++ src/app/components/teacher/homework/give-homework/give-homework.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-give-homework',
+  standalone: true,
+  imports: [],
+  templateUrl: './give-homework.component.html',
+  styleUrl: './give-homework.component.css'
+})
+export class GiveHomeworkComponent {
+
+}
