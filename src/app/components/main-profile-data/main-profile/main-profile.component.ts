35df63ebebbd551d846f4c6d70e7e7fee7d2bf76
Alok Kumar Singh <i.am.alok2513@gmail.com>
2024-08-02 21:01:48 +0530

    profile component is added

---
+++ src/app/components/main-profile-data/main-profile/main-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-profile.component.html',
+  styleUrl: './main-profile.component.css'
+})
+export class MainProfileComponent {
+
+}
