d82c219e58c21a2138ee264e3a4407d4ea5f7860
Alok Kumar Singh <i.am.alok2513@gmail.com>
2024-11-07 00:53:34 +0530

    notification is added

--- src/app/components/student/navbar/navbar.component.ts
+++ src/app/components/student/navbar/navbar.component.ts
@@ -1,19 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MainProfileComponent } from "../../main-profile-data/main-profile/main-profile.component";
+import { MainProfileComponent } from '../../main-profile-data/main-profile/main-profile.component';
 import { ProfileCardComponent } from '../profile-card/profile-card.component';
+import { CommonModule } from '@angular/common';
+import { NoficationComponent } from "../../nofication/nofication.component";
+import { StudentNotificationComponent } from "../../Admin/student-notification/student-notification.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MainProfileComponent],
+  imports: [RouterLink, RouterLinkActive, MainProfileComponent, CommonModule, NoficationComponent, StudentNotificationComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent  {
-  show=false;
+export class NavbarComponent {
+   
+   isProfileOpen = false;
+   isNotificationOpen = false;
  
-openProfile(){
-this.show=!this.show;
-  }
+   toggleProfile() {
+
+     if (this.isNotificationOpen) {
+       this.isNotificationOpen = false;
+     }
+    
+     this.isProfileOpen = !this.isProfileOpen;
+   }
+ 
+   
+   toggleNotification() {
+  
+     if (this.isProfileOpen) {
+       this.isProfileOpen = false;
+     }
+   
+     this.isNotificationOpen = !this.isNotificationOpen;
+   }
+ 
+   openNotification() {
+     console.log('Notification opened');
+   }
 }
