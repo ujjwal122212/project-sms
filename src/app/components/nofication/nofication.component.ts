d7b66609f9da35a83127040f6c7ec202cd933fc3
Alok Kumar Singh <i.am.alok2513@gmail.com>
2024-11-08 21:44:17 +0530

    Notification student

--- src/app/components/nofication/nofication.component.ts
+++ src/app/components/nofication/nofication.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nofication',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nofication.component.html',
-  styleUrl: './nofication.component.css'
+  styleUrls: ['./nofication.component.css'],
 })
 export class NoficationComponent {
+  notificationStudent: any[] = [];
 
+  constructor(private http: HttpClient) {
+    this.getNotificationOfStudent();
+  }
+
+  getNotificationOfStudent(): void {
+    this.http
+      .get<any[]>(
+        'https://localhost:7262/api/StudentsNotification/GetStudentNotification'
+      )
+      .subscribe((res) => {
+        this.notificationStudent = res;
+      });
+  }
 }
