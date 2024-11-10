46516b3f89b0a5e3f5ff1ec6313e3027b563e76d
Ujjwal raj <ujjwalr658@gmail.com>
2024-11-10 20:56:35 +0530

    adding routes for fee

--- src/app/components/student/navbar/navbar.component.ts
+++ src/app/components/student/navbar/navbar.component.ts
@@ -8,41 +8,40 @@ import { StudentProfileComponent } from '../student-profile/student-profile.comp
 import { CommonModule } from '@angular/common';
 import { NoficationComponent } from "../../nofication/nofication.component";
 import { StudentNotificationComponent } from "../../Admin/student-notification/student-notification.component";
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-
-  imports: [RouterLink, RouterLinkActive, MainProfileComponent,StudentProfileComponent],
-
+  imports: [RouterLink,FormsModule,CommonModule, RouterLinkActive, MainProfileComponent, StudentProfileComponent, NoficationComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-   
+
    isProfileOpen = false;
    isNotificationOpen = false;
- 
+
    toggleProfile() {
 
      if (this.isNotificationOpen) {
        this.isNotificationOpen = false;
      }
-    
+
      this.isProfileOpen = !this.isProfileOpen;
    }
- 
-   
+
+
    toggleNotification() {
-  
+
      if (this.isProfileOpen) {
        this.isProfileOpen = false;
      }
-   
+
      this.isNotificationOpen = !this.isNotificationOpen;
    }
- 
+
    openNotification() {
      console.log('Notification opened');
    }
