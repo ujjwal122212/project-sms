ed0202f5093e1baeafd45dcb6cd4b26090bb1ae4
Md Taufique <mdtaufique8084@gmail.com>
2024-11-20 22:01:51 +0530

    add profile

--- src/app/components/teacher/teacher-profile/teacher-profile.component.ts
+++ src/app/components/teacher/teacher-profile/teacher-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../../../Services/login.service';
 
 @Component({
   selector: 'app-teacher-profile',
@@ -8,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrl: './teacher-profile.component.css'
 })
 export class TeacherProfileComponent {
-
+ route=inject(Router);
+ loginService = inject(LoginService);
+ logout() {
+  this.loginService.logout();
+  this.route.navigateByUrl('/Login-page');
+}
 }
