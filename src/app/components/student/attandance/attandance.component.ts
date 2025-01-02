0153f59d1c54b24ee726e8769d5f3fa84f6b40e6
Md Taufique <mdtaufique8084@gmail.com>
2025-01-03 04:08:44 +0530

    student attence on student page

--- src/app/components/student/attandance/attandance.component.ts
+++ src/app/components/student/attandance/attandance.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LoginService } from '../../../Services/login.service';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-attandance',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './attandance.component.html',
-  styleUrl: './attandance.component.css'
+  styleUrls: ['./attandance.component.css'] // Corrected to styleUrls
 })
-export class AttandanceComponent {
+export class AttandanceComponent implements OnInit {
+  isLoading: boolean = true;
+  errorMessage: string = '';
+  enrollmentNo!: number | null;
+  attendanceRecords: any[] = [];
+  loginService = inject(LoginService);
+  htpclient = inject(HttpClient);
 
+  getStudentAttendanceByEnrollmentNumber(enrollmentNo: number) {
+    this.htpclient
+      .get(`https://localhost:7262/GetStudentAttendance/${enrollmentNo}`)
+      .subscribe(
+        (res: any) => {
+          // Sorting the attendence by date in ascending order
+          this.attendanceRecords = res.sort((a: any, b: any) => {
+            return new Date(a.attendanceDate).getTime() - new Date(b.attendanceDate).getTime();
+          });
+          this.isLoading = false;
+        },
+        (error) => {
+          this.errorMessage = 'An error occurred while fetching data.';
+          this.isLoading = false;
+        }
+      );
+  }
+  
+
+  ngOnInit(): void {
+    this.enrollmentNo = this.loginService.enrollmentNumber;
+    if (this.enrollmentNo) {
+      this.getStudentAttendanceByEnrollmentNumber(this.enrollmentNo);
+    }
+  }
 }
