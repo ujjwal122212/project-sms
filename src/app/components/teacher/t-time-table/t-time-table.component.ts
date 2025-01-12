ddffd6080426e743d2a2348067723b25ff9ffb9f
Md Taufique <mdtaufique8084@gmail.com>
2025-01-12 12:23:34 +0530

    teacher attendance

--- src/app/components/teacher/t-time-table/t-time-table.component.ts
+++ src/app/components/teacher/t-time-table/t-time-table.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { LoginService } from '../../../Services/login.service';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-t-time-table',
   standalone: true,
-  imports: [RouterLink ,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,CommonModule],
   templateUrl: './t-time-table.component.html',
   styleUrl: './t-time-table.component.css'
 })
-export class TTimeTableComponent {
+export class TTimeTableComponent implements OnInit {
+  enrollmentNo!: number | null;
+  route = inject(Router);
+  loginService = inject(LoginService);
+  http = inject(HttpClient);
+  teacherTimeTable: any[] = []
+  getTimeTableByTeacherEnrollmentNumber(enrollmentNumber: number) {
+    this.http.get(`https://localhost:7262/GetTeacherTimeTable?teacherId=${enrollmentNumber}`).subscribe((res: any) => {
+      this.teacherTimeTable = res;
+    })
+  }
+  ngOnInit(): void {
+    this.enrollmentNo = this.loginService.enrollmentNumber;
+    if (this.enrollmentNo) {
+      console.log("yha par", this.enrollmentNo);
+      this.getTimeTableByTeacherEnrollmentNumber(this.enrollmentNo);
+    }
+  }
 
 }
