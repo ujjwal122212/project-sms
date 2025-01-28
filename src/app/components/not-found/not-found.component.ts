bc70eba717c8f70bdd58009a167d995a52869111
Md Taufique <mdtaufique8084@gmail.com>
2025-01-28 20:13:23 +0530

    unathorized changed

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-not-found',
   standalone: true,
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
-
+  router=inject(Router)
+  gotoHome(){
+    this.router.navigateByUrl('/Login-page');
+  }
 }
