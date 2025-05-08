7e6c1afeb48d21351419378c6f9d1ac25e061e5c
Ujjwal <ujjwalr658@gmail.com>
2025-05-08 23:21:36 +0530

    fee structure

--- src/app/components/Admin/fee/create-fee-structure/create-fee-structure.component.ts
+++ src/app/components/Admin/fee/create-fee-structure/create-fee-structure.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-fee-structure',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './create-fee-structure.component.html',
   styleUrl: './create-fee-structure.component.css'
 })
--- src/app/components/Admin/fee/show-fee-structure/show-fee-structure.component.ts
+++ src/app/components/Admin/fee/show-fee-structure/show-fee-structure.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-show-fee-structure',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,RouterLink],
   templateUrl: './show-fee-structure.component.html',
   styleUrl: './show-fee-structure.component.css'
 })
 export class ShowFeeStructureComponent {
 
+  feeStructureList:any[]=[];
+
+  constructor(private http:HttpClient){
+     this.getFeeStructure();
+  }
+
+  getFeeStructure(){
+    this.http.get("https://localhost:7262/api/FeeStructure/GetAllFeeStructure").subscribe((result:any)=>{
+      this.feeStructureList=result;
+      
+    })
+  }
+
+  deleteFeeStructure(id:number){
+    const Url='https://localhost:7262/api/FeeStructure/DeleteFeeStructure/'+id;
+    if (confirm('Do you want to delete this item?')) {
+    
+    this.http.delete(Url,{responseType:'text'}).subscribe(result=>{
+      alert(result);
+      this.getFeeStructure();
+    })
+  }
+  else{
+    alert('Not deleted');
+  }
+  }
 }
