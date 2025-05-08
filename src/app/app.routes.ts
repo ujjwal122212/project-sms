e9ab6eebab0bda7fd8edd6f4789d0815a2eff152
Ujjwal <ujjwalr658@gmail.com>
2025-05-08 19:16:08 +0530

    adding list of free structure table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -94,6 +94,7 @@ import { ParentInfoComponent } from './components/Admin/parent-info/parent-info.
 import { ViewAllStudentComponent } from './components/Admin/addStudent/view-all-student/view-all-student.component';
 import { StudentAttendenceSummeryComponent } from './components/Admin/student-attendence-summery/student-attendence-summery.component';
 import { CreateFeeStructureComponent } from './components/Admin/fee/create-fee-structure/create-fee-structure.component';
+import { ShowFeeStructureComponent } from './components/Admin/fee/show-fee-structure/show-fee-structure.component';
 // import { RegisterTeacherComponent } from './components/Admin/register-teacher/register-teacher.component';
 // import { ResetPasswordComponent } from './main-profile/reset-password/reset-password.component';
 
@@ -1330,6 +1331,13 @@ export const routes: Routes = [
         path: 'createstudentfee',
         title: 'Student Fee Structure',
         component: CreateFeeStructureComponent,
+        canActivate: [authGuard],
+        data: { roles: ['Admin'] },
+      },
+      {
+        path: 'ShowstudentfeeStructure',
+        title: 'Student Fee Structure',
+        component: ShowFeeStructureComponent,
         canActivate: [authGuard],
         data: { roles: ['Admin'] },
       },
---
+++ src/app/components/Admin/fee/show-fee-structure/show-fee-structure.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-fee-structure',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-fee-structure.component.html',
+  styleUrl: './show-fee-structure.component.css'
+})
+export class ShowFeeStructureComponent {
+
+}
